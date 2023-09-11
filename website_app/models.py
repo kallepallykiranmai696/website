5b5f9406c43fffa8acc5b9e7092eefa7eb1363f1
kallepallykiranmai696 <142095646+kallepallykiranmai696@users.noreply.github.com>
2023-09-11 16:50:02 +0530

    Update models.py

--- website_app/models.py
+++ website_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name=models.CharField(max_length=30)
-    phone_number=models.IntegerField()
+    phone_number=models.BigIntegerField()
     email=models.EmailField()
     select_course=models.CharField(max_length=30)
     def __str__(self):
